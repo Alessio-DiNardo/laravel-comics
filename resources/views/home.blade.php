a09990405760efe07e5395f24c8c2ec9c2ebc6dd
Alessio-DiNardo <Alessiodinardo9@gmail.comm>
2023-07-23 15:07:58 +0200

    add foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,21 @@
 @extends('layouts.app')
 
 @section('main-content')
-    <div class="container">
+    <div class="container-fluid bg-dark">
         <div class="row">
-            <div class="col-12">
-                <h1>Hello World from main content</h1>
-            </div>
+            @foreach ($comics as $comic)
+                <div class="col-2 py-3">
+                    <div class="card">
+                    <img src="{{ $comic['thumb']}}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">{{$comic['title']}}</p>
+                        <p class="card-text">{{$comic['description']}}</p>
+                        <p class="card-text">{{$comic['price']}}</p>
+                        <p class="card-text">{{$comic['type']}}</p>
+                    </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
